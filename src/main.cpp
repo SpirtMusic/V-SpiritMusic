eecab10faa43e75060e2706887625abb1a49e76b
Akram Abeslem Chaima <akram@riseup.net>
2024-05-11 23:24:24 +0100

    using qt_add_qml_module instead global resource

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
-    const QUrl url(QStringLiteral("qrc:/vsonegx/Main.qml"));
+    const QUrl url(QStringLiteral("qrc:/vsonegx/qml/Main.qml"));
     QObject::connect(
         &engine,
         &QQmlApplicationEngine::objectCreationFailed,
