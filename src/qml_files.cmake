7ff2ee12a36c34189d8be50572f382fc2c69b709
Akram Abeslem Chaima <akram@riseup.net>
2024-06-22 22:38:23 +0100

    separate resources and qml files from main CMakefile

---
+++ src/qml_files.cmake
@@ -0,0 +1,23 @@
+set(QML_FILES
+    qml/Main.qml
+    qml/themes/Theme.qml
+    qml/controls/VTabButton.qml
+    qml/layouts/VTabBar.qml
+    qml/controls/VLayerButton.qml
+    qml/layouts/layers/VLayerContainer.qml
+    qml/controls/VLayerControl.qml
+    qml/views/Layers.qml
+    qml/layouts/layers/VLayersControlContainer.qml
+    qml/controls/VGroupBox.qml
+    qml/controls/VKnob.qml
+    qml/controls/VSplitView.qml
+    qml/views/Settings.qml
+    qml/layouts/settings/TabBarSettings.qml
+    qml/controls/VSlider.qml
+    qml/layouts/settings/settingsPages/ConnectionsPage.qml
+    qml/controls/VComboBox.qml
+    qml/controls/VButton.qml
+    qml/controls/VPopup.qml
+    qml/controls/VSliderPopup.qml
+    qml/controls/VSliderPopupH.qml
+)
---
+++ src/resources.cmake
@@ -0,0 +1,91 @@
+set(RESOURCE_FILES
+    qml/imgs/music.svg
+    qml/imgs/sliders-h.svg
+    qml/imgs/wrench.svg
+    qml/imgs/cog.svg
+    qml/imgs/midi.svg
+    qml/imgs/info-circle.svg
+    qml/imgs/th.svg
+    qml/imgs/pencil-ruler.svg
+   # qml/controls/resource/knob/Aqua/Aqua_128.png
+    # qml/controls/resource/knob/Aqua/Aqua_30.png
+    # qml/controls/resource/knob/Aqua/Aqua_40.png
+    # qml/controls/resource/knob/Aqua/Aqua_50.png
+    # qml/controls/resource/knob/Aqua/Aqua_85.png
+    # qml/controls/resource/knob/Bluesbreaker/Bluesbreaker_128.png
+    # qml/controls/resource/knob/Bluesbreaker/Bluesbreaker_30.png
+    # qml/controls/resource/knob/Bluesbreaker/Bluesbreaker_40.png
+    # qml/controls/resource/knob/Bluesbreaker/Bluesbreaker_50.png
+    # qml/controls/resource/knob/Bluesbreaker/Bluesbreaker_85.png
+    # qml/controls/resource/knob/Credence/Credence_128.png
+    # qml/controls/resource/knob/Credence/Credence_30.png
+    # qml/controls/resource/knob/Credence/Credence_40.png
+    # qml/controls/resource/knob/Credence/Credence_50.png
+    # qml/controls/resource/knob/Credence/Credence_85.png
+    # qml/controls/resource/knob/FStyle/FStyle_128.png
+    # qml/controls/resource/knob/FStyle/FStyle_30.png
+    # qml/controls/resource/knob/FStyle/FStyle_40.png
+    # qml/controls/resource/knob/FStyle/FStyle_50.png
+    # qml/controls/resource/knob/FStyle/FStyle_85.png
+    # qml/controls/resource/knob/Hexagonal/Hexagonal_128.png
+    # qml/controls/resource/knob/Hexagonal/Hexagonal_30.png
+    # qml/controls/resource/knob/Hexagonal/Hexagonal_40.png
+    # qml/controls/resource/knob/Hexagonal/Hexagonal_50.png
+    # qml/controls/resource/knob/Hexagonal/Hexagonal_85.png
+    # qml/controls/resource/knob/Hippy/Hippy_128.png
+    # qml/controls/resource/knob/Hippy/Hippy_30.png
+    # qml/controls/resource/knob/Hippy/Hippy_40.png
+    # qml/controls/resource/knob/Hippy/Hippy_50.png
+    # qml/controls/resource/knob/Hippy/Hippy_85.png
+    # qml/controls/resource/knob/kjLED/kjLED_128.png
+    # qml/controls/resource/knob/kjLED/kjLED_30.png
+    # qml/controls/resource/knob/kjLED/kjLED_40.png
+    # qml/controls/resource/knob/kjLED/kjLED_50.png
+    # qml/controls/resource/knob/kjLED/kjLED_85.png
+    qml/controls/resource/knob/LittlePhatty/LittlePhatty_128.png
+    qml/controls/resource/knob/LittlePhatty/LittlePhatty_30.png
+    qml/controls/resource/knob/LittlePhatty/LittlePhatty_40.png
+    qml/controls/resource/knob/LittlePhatty/LittlePhatty_50.png
+    qml/controls/resource/knob/LittlePhatty/LittlePhatty_85.png
+    # qml/controls/resource/knob/Oscar/Oscar_128.png
+    # qml/controls/resource/knob/Oscar/Oscar_30.png
+    # qml/controls/resource/knob/Oscar/Oscar_40.png
+    # qml/controls/resource/knob/Oscar/Oscar_50.png
+    # qml/controls/resource/knob/Oscar/Oscar_85.png
+    # qml/controls/resource/knob/RedScale/RedScale_128.png
+    # qml/controls/resource/knob/RedScale/RedScale_30.png
+    # qml/controls/resource/knob/RedScale/RedScale_40.png
+    # qml/controls/resource/knob/RedScale/RedScale_50.png
+    # qml/controls/resource/knob/RedScale/RedScale_85.png
+    # qml/controls/resource/knob/Silver/Silver_128.png
+    # qml/controls/resource/knob/Silver/Silver_30.png
+    # qml/controls/resource/knob/Silver/Silver_40.png
+    # qml/controls/resource/knob/Silver/Silver_50.png
+    # qml/controls/resource/knob/Silver/Silver_85.png
+    # qml/controls/resource/knob/SSLish/SSLish_128.png
+    # qml/controls/resource/knob/SSLish/SSLish_30.png
+    # qml/controls/resource/knob/SSLish/SSLish_40.png
+    # qml/controls/resource/knob/SSLish/SSLish_50.png
+    # qml/controls/resource/knob/SSLish/SSLish_85.png
+    # qml/controls/resource/knob/Vintage/Vintage_128.png
+    # qml/controls/resource/knob/Vintage/Vintage_30.png
+    # qml/controls/resource/knob/Vintage/Vintage_40.png
+    # qml/controls/resource/knob/Vintage/Vintage_50.png
+    # qml/controls/resource/knob/Vintage/Vintage_85.png
+    # qml/controls/resource/knob/Wedge/Wedge_128.png
+    # qml/controls/resource/knob/Wedge/Wedge_30.png
+    # qml/controls/resource/knob/Wedge/Wedge_40.png
+    # qml/controls/resource/knob/Wedge/Wedge_50.png
+    # qml/controls/resource/knob/Wedge/Wedge_85.png
+
+    qml/controls/resource/texture/tx3.jpg
+    qml/controls/resource/effects/fog_effect.png
+    qml/controls/resource/effects/onLayer.png
+    qml/controls/resource/texture/button_texture.png
+    qml/controls/resource/texture/main.png
+
+    qml/controls/resource/slider/handler.png
+    qml/controls/resource/slider/slider_handler_H.png
+    qml/controls/resource/slider/slider_handler_V.png
+
+)
